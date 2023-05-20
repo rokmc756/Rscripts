70512b9a37da7bd856a2d5631e4cd015e0b9ba62
Jack Moon <rokmc756@gmail.com>
2023-05-20 23:31:08 +0900

    Added README.md

--- ctrl-r-pkgs.rs
+++ ctrl-r-pkgs.rs
@@ -132,12 +132,3 @@ if ( !is.null(opt$package) && !is.null(opt$operation) ) {
 
 # signal success and exit.
 # q(status=0)
-
-# Install R binardy 
-# https://docs.posit.co/resources/install-r/
-# https://docs.posit.co/rspm/admin/repositories/#multiple-sources
-# ttps://docs.posit.co/rspm/admin/appendix/system-dependency-detection/
-# https://github.com/rstudio/r-builds
-# https://www.rdocumentation.org/packages/utils/versions/3.6.2/topics/install.packages
-# https://cran.r-project.org/bin/windows/base/howto-R-devel.html
-# https://cran.r-project.org/doc/manuals/r-release/R-admin.html
